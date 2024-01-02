e5414e68a745e0d428eb66811dd26fe59e5f1b9d
Richard <code.monkey.ricky@gmail.com>
2024-01-01 18:49:23 -0800

    matrix class

---
+++ src/helpers/matrix.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <array>
+#include <cmath>
+
+struct alignas(16) vector4i 
+{
+    std::array<int, 4> data = {};
+    int operator [](int i) const {return data[i];}
+    int & operator [](int i) {return data[i];}
+}; 
+
+struct alignas(16) vector4f 
+{
+    std::array<float, 4> data = {};
+    float operator [](int i) const {return data[i];}
+    float & operator [](int i) {return data[i];}
+
+    inline float dot(const vector4f &op) const
+    {
+        float rv = 0;
+        for (auto i = 0; i < 4; ++i)
+            rv += data[i] * op[i];
+        return rv;
+    }
+
+    inline vector4f operator*(const float &op) const
+    {
+        vector4f rv = {};
+        for (auto i = 0; i < 4; ++i)
+            rv[i] = data[i] * op;
+        return rv; 
+    }
+
+    inline vector4f operator +(const vector4f &op) const
+    {
+        vector4f rv = {};
+        for (auto i = 0; i < 4; ++i)
+            rv[i] = data[i] + op[i];
+        return rv; 
+    }
+
+    inline vector4f operator-(const vector4f &op) const
+    {
+        auto a = _mm_load_ps((const float *)&data);
+        auto b = _mm_load_ps((const float *)&op);
+        auto c = _mm_sub_ps(a, b);
+        vector4f rv;
+        _mm_store_ps((float *)&rv, c);
+        return rv;
+    }
+
+    inline float squaredNorm() const
+    {
+        float rv = 0;
+        for (auto i = 0; i < 4; ++i)
+            rv += data[i] * data[i];
+        return rv;
+    }
+
+    inline float norm() const
+    {
+        float rv = 0;
+        for (auto i = 0; i < 4; ++i)
+            rv += data[i] * data[i];
+        return std::sqrt(rv); 
+    }
+}; 
+
+struct alignas(16) matrix4f
+{
+    std::array<vector4f, 4> data = {};
+    vector4f operator [](int i) const {return data[i];}
+    vector4f & operator [](int i) {return data[i];}
+
+    inline vector4f operator *(const vector4f &op) const
+    {
+        vector4f rv = {};
+        for (auto i = 0; i < 4; ++i)
+            for (auto j = 0; j < 4; ++j)
+                rv[i] += data[i][j] * op[j];
+        return rv; 
+    }
+};
+
+inline vector4f operator*(matrix4f &&c1, vector4f &&c2);
+
+inline vector4f operator+(vector4f const &&c1, vector4f const &&c2);
+// inline vector4f operator+(vector4f &c1, vector4f const &c2);
+// inline vector4f operator+(vector4f const &c1, vector4f &c2);
+// inline vector4f operator+(vector4f &c1, vector4f &c2);
