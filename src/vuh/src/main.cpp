989cfffbddb6c1de45894f3f400053135b445d62
Richard <code.monkey.ricky@gmail.com>
2024-03-24 12:44:19 -0700

    wip

--- src/vuh/src/main.cpp
+++ src/vuh/src/main.cpp
@@ -12,9 +12,10 @@
 auto main()-> int {
    int len = 256; 
    int w = 640, h = 480;
+   int total = len * len * len/4; 
    auto depth = std::vector<float>(w * h, 1.0f);
-   auto tsdf = std::vector<float>(len * len * len, 1.0f);
-   auto weight = std::vector<float>(len * len * len, 2.0f);
+   auto tsdf = std::vector<uint32_t>(total, 0x7f7f7f7f);
+   auto weight = std::vector<uint32_t>(total, 0x01010101);
 
    auto instance = vuh::Instance();
    auto device = instance.devices().at(0);    // just get the first available device
@@ -22,8 +23,8 @@ auto main()-> int {
    auto t0 = std::chrono::high_resolution_clock::now();
 
    auto d_d = vuh::Array<float>(device, depth);   // create device arrays and copy data
-   auto d_y = vuh::Array<float>(device, tsdf);   // create device arrays and copy data
-   auto d_x = vuh::Array<float>(device, weight);
+   auto d_y = vuh::Array<uint32_t>(device, tsdf);   // create device arrays and copy data
+   auto d_x = vuh::Array<uint32_t>(device, weight);
 
    auto t1 = std::chrono::high_resolution_clock::now();
 
@@ -41,7 +42,7 @@ auto main()-> int {
    matrix4f rot;
    vector4f trans; 
    matrix4f intrs; 
-   program.grid(32, 32,8).spec(64)({rot, trans, intrs, w, h, len}, d_d, d_y, d_x); // run once, wait for completion
+   program.grid(total /64).spec(64)({rot, trans, intrs, w, h, len}, d_d, d_y, d_x); // run once, wait for completion
 
    auto t2 = std::chrono::high_resolution_clock::now();
 
@@ -53,8 +54,9 @@ auto main()-> int {
    std::cout << "Compute: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms" << std::endl;
    std::cout << "Download: " << std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count() << " ms" << std::endl;
 
-   for(auto i = 0; i < len * len * len; ++i)
-      assert(tsdf[i] == i);
+   uint8_t *tsdf_ptr = (uint8_t*)&tsdf[0];
+   for(auto i = 0; i < total*4; ++i)
+      assert(tsdf_ptr[i] == i % 256);
 
    return 0;
 }
